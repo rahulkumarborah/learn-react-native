5dbc77e55312e7a0ae08c19fea89f560036de7e3
Rahul Kumar Borah <rahulkumarborah@gmail.com>
2021-12-29 19:00:11 +0530

    Form exercise part 1

--- src/screens/ColorPaletteModal.js
+++ src/screens/ColorPaletteModal.js
@@ -1,13 +1,65 @@
-import { Text, View } from 'react-native';
-
-import React from 'react';
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useCallback, useState } from 'react';
 
 const ColorPaletteModal = () => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = useCallback(() => {
+    if (!name) {
+      Alert.alert('Please enter a palette name');
+    }
+  }, [name]);
+
   return (
-    <View>
-      <Text>Hello, I am a modal</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>Name of the palette</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Palette name"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 30,
+  },
+  container: {
+    padding: 10,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  name: {
+    marginBottom: 10,
+  },
+});
 
 export default ColorPaletteModal;
